e697ab9eabbf979f7b77cc8d2d7e0d36879ecc5e
Zachary Kline <zkline888@gmail.com>
2019-11-21 15:11:40 -0500

    Committing finished twitterclone project with styling

--- twitterclone/notification/views.py
+++ twitterclone/notification/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from twitterclone.twitteruser.models import TwitterUser
 # from twitterclone.tweet.models import Tweet
 from twitterclone.notification.models import Notifications
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def notificationpage(request, id):
     html = 'notification/notify.html'
     twitteruser = TwitterUser.objects.filter(id=id).first()
